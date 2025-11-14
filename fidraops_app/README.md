960673eb40d75e5a5c7930fb4c8a00fda5c9bd9e
Claudiu Spiescu <claudiu.spiescu@redevia.ro>
2025-11-14 15:02:12 +0200

    Initial commit

---
+++ fidraops_app/README.md
@@ -0,0 +1,3 @@
+# fidraops_app
+
+A new Flutter project.
---
+++ fidraops_app/android/app/src/main/kotlin/com/example/fidraops_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.fidraops_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
